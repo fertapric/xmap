fbea827432a0f504b8b868bffbc4d46bb86b514f
Fernando Tapia Rico <fertapric@gmail.com>
2017-07-31 21:13:37 +0200

    Apply new mix.exs styles

--- mix.exs
+++ mix.exs
@@ -4,23 +4,29 @@ defmodule XMap.Mixfile do
   @version "0.2.1"
 
   def project do
-    [app: :xmap,
-     name: "XMap",
-     version: @version,
-     elixir: "~> 1.4",
-     deps: deps(),
-     package: package(),
-     description: description(),
-     docs: docs()]
+    [
+      app: :xmap,
+      name: "XMap",
+      version: @version,
+      elixir: "~> 1.4",
+      deps: deps(),
+      package: package(),
+      description: description(),
+      docs: docs()
+   ]
   end
 
   def application do
-    [extra_applications: [:xmerl]]
+    [
+      extra_applications: [:xmerl]
+    ]
   end
 
   defp deps do
-    [{:ex_doc, "~> 0.14", only: [:test, :docs], runtime: false},
-     {:inch_ex, ">= 0.0.0", only: [:dev, :docs], runtime: false}]
+    [
+      {:ex_doc, "~> 0.14", only: [:test, :docs], runtime: false},
+      {:inch_ex, ">= 0.0.0", only: [:dev, :docs], runtime: false}
+    ]
   end
 
   defp description do
@@ -30,16 +36,20 @@ defmodule XMap.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Fernando Tapia Rico"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/fertapric/xmap"},
-     files: ~w(mix.exs README.md lib)]
+    [
+      maintainers: ["Fernando Tapia Rico"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/fertapric/xmap"},
+      files: ~w(mix.exs README.md lib)
+    ]
   end
 
   defp docs do
-    [source_ref: "v#{@version}",
-     main: "XMap",
-     canonical: "http://hexdocs.pm/xmap",
-     source_url: "https://github.com/fertapric/xmap"]
+    [
+      source_ref: "v#{@version}",
+      main: "XMap",
+      canonical: "http://hexdocs.pm/xmap",
+      source_url: "https://github.com/fertapric/xmap"
+    ]
   end
 end
